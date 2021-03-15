994b33bf9f3a92d4f39cc0af839a1c0d6560c965
Narendra Kumar <30648034+nar3n12@users.noreply.github.com>
2021-03-15 21:54:43 +0530

    Initial commit
    No AR (yet)
    Just portal using Stencils
    A script to monitor whether inside/outside portal
    Change visibility based on camera positon
    sleepy... good night

---
+++ Assets/Scripts/InterdimensionalTransport.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class InterdimensionalTransport : MonoBehaviour
+{
+
+    public Material[] materials;
+    
+    // Start is called before the first frame update
+   private void Start()
+    {
+        Debug.Log("started");
+        foreach (var mat in materials)
+        {
+            mat.SetInt("_StencilTest",(int)CompareFunction.Equal);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name != "Main Camera") 
+            return;
+        
+        //outside of other world aka portal
+        if (transform.position.z > other.transform.position.z)
+        {
+            Debug.Log("outside portal");
+            foreach (var mat in materials)
+            {
+                mat.SetInt("_StencilTest",(int)CompareFunction.Equal);
+            }
+        }
+        //inside portal aka other world
+        else   
+        {
+            Debug.Log("inside portal");
+            foreach (var mat in materials)
+            {
+                mat.SetInt("_StencilTest",(int)CompareFunction.NotEqual);
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        foreach (var mat in materials)
+        {
+            mat.SetInt("_StencilTest",(int)CompareFunction.NotEqual);
+        }
+    }
+
+   
+}
